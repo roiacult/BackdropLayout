6286c9aadbe7d0a56cfcdfdade787915dc433330
roiacult <roiacult27@gmail.com>
2019-12-21 12:37:36 +0100

    testing and preparing for new updates

--- app/src/main/java/com/roacult/kero/team7/backdropapp/MainActivity.kt
+++ app/src/main/java/com/roacult/kero/team7/backdropapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.roacult.kero.team7.backdropapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.search.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search)
+        container.setOnBackdropChangeStateListener {
+            Toast.makeText(this,"backdrop change state to : $it ",Toast.LENGTH_SHORT).show()
+        }
     }
 }
