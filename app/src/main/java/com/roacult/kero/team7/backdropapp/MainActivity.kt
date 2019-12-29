a35ca317f9387b7823636fd44394c1e138017624
roiacult <roiacult27@gmail.com>
2019-12-29 12:23:25 +0100

    testing new features

--- app/src/main/java/com/roacult/kero/team7/backdropapp/MainActivity.kt
+++ app/src/main/java/com/roacult/kero/team7/backdropapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.search.*
+import kotlinx.android.synthetic.main.search_product.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search)
-        container.setOnBackdropChangeStateListener {
-            Toast.makeText(this,"backdrop change state to : $it ",Toast.LENGTH_SHORT).show()
+//        container.setOnBackdropChangeStateListener {
+//            Toast.makeText(this,"backdrop change state to : $it ",Toast.LENGTH_SHORT).show()
+//        }
+        bla.setOnClickListener {
+            Toast.makeText(this,"textView is Clicked",Toast.LENGTH_SHORT).show()
         }
     }
 }
